f3b5fc6c512a487908855d3f6fade16d3e25e8d0
dalex1981a <168557868+dalex1981a@users.noreply.github.com>
2024-07-25 00:55:11 +0000

    dunno

---
+++ src/app/about-us/page.js
@@ -0,0 +1,11 @@
+export default function AboutUs(){
+    return (
+        <div className="container mx-auto">
+            <h1 className="text-2xl font-semibold"> About us</h1>
+            <p>My name is David Garcia</p>
+            <p>
+      <a href="./">Home</a>
+    </p>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className='container mx-auto text-xl'>Hello MDC27</div>
+    <div className='container mx-auto text-xl'>Hello DAG APP
+    <p>
+      <a href="/login">Login</a>
+    </p>
+    <p>
+      <a href="/about-us">About us</a>
+    </p>
+    </div>
   );
 }
